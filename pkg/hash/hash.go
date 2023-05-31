16b17f9b83509ae3132c3c2e36f62dbd11a1c2a6
Maciej Galeja <maciej.galeja@outlook.com>
2023-05-31 16:18:16 +0200

    Add functions to hash various inputs

--- pkg/hash/hash.go
+++ pkg/hash/hash.go
@@ -1,20 +1,41 @@
 package hash
 
 import (
+	"bytes"
 	"crypto/sha512"
 	"fmt"
 	"io"
+	"os"
 	"strings"
+
+	"github.com/maciejgaleja/gosimple/pkg/types"
 )
 
-type Hash []byte
+type Sha512 []byte
 
-func HashString(s string) Hash {
+func Hash(r io.Reader) Sha512 {
 	h := sha512.New()
-	r := strings.NewReader(s)
 	_, err := io.Copy(h, r)
 	if err != nil {
 		panic(err)
 	}
-	return Hash(fmt.Sprintf("%x", h.Sum(nil)))
+	return Sha512(fmt.Sprintf("%x", h.Sum(nil)))
+}
+
+func HashBytes(d []byte) Sha512 {
+	return Hash(bytes.NewBuffer(d))
+}
+
+func HashString(s string) Sha512 {
+	r := strings.NewReader(s)
+	return Hash(r)
+}
+
+func HashFile(f types.FilePath) (Sha512, error) {
+	fd, err := os.Open(string(f))
+	if err != nil {
+		return Sha512{}, err
+	}
+
+	return Hash(fd), nil
 }
