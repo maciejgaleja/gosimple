9e83179f69b30072bd1ae0784764a5f6bc5d048b
Maciej Galeja <maciej.galeja@outlook.com>
2023-07-08 08:08:41 +0000

    Add tests for dynamodb

--- pkg/nosql/dynamodb/dynamodb_test.go
+++ pkg/nosql/dynamodb/dynamodb_test.go
@@ -1,16 +1,26 @@
 package dynamodb_test
 
-// func TestKeyValue(t *testing.T) {
-// 	test.DoTestKeyValue(t, func() keyvalue.Store {
-// 		sess, err := session.NewSessionWithOptions(session.Options{
-// 			SharedConfigState: session.SharedConfigEnable,
-// 			Profile:           "default",
-// 		})
-// 		assert.NoError(t, err)
+import (
+	"testing"
 
-// 		ddb := dynamodb.NewDynamoDb(sess, "gosimple-test", "key")
-// 		err = ddb.Clear()
-// 		assert.NoError(t, err)
-// 		return ddb
-// 	})
-// }
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/maciejgaleja/gosimple/pkg/nosql"
+	"github.com/maciejgaleja/gosimple/pkg/nosql/dynamodb"
+	"github.com/maciejgaleja/gosimple/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDynamoDb(t *testing.T) {
+	test.DoTestNoSql(t, func() nosql.Store {
+		sess, err := session.NewSessionWithOptions(session.Options{
+			SharedConfigState: session.SharedConfigEnable,
+			Profile:           "default",
+		})
+		assert.NoError(t, err)
+
+		ddb := dynamodb.NewDynamoDb(sess, "gosimple-test", "key")
+		err = ddb.Clear()
+		assert.NoError(t, err)
+		return ddb
+	})
+}
